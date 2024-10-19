6b3c0f6abfa30ef88d1b5c8265644ab3ec2ef3f3
jjrboucher <jjrboucher@users.noreply.github.com>
2024-10-19 08:33:37 -0400

    reformatted the query. No change to the actual query.

--- SQLite/searchterms.py
+++ SQLite/searchterms.py
@@ -1,10 +1,16 @@
 def chrome_keyword_historyquery():
-    history_query = """SELECT	urls.id, urls.url, keyword_search_terms.keyword_id, keyword_search_terms.url_id, 
-    keyword_search_terms.term, urls.visit_count, urls.last_visit_time, datetime(
-    urls.last_visit_time/1000000-11644473600,'unixepoch') AS "Decoded history.last_visit_time (UTC)"
+    history_query = """
+        SELECT	urls.id, 
+        urls.url, 
+        keyword_search_terms.keyword_id, 
+        keyword_search_terms.url_id, 
+        keyword_search_terms.term, 
+        urls.visit_count, 
+        urls.last_visit_time, 
+        datetime(urls.last_visit_time/1000000-11644473600,'unixepoch') AS "Decoded history.last_visit_time (UTC)"
                     
         FROM urls
-            LEFT JOIN keyword_search_terms ON urls.id=keyword_search_terms.url_id
+        LEFT JOIN keyword_search_terms ON urls.id=keyword_search_terms.url_id
     """
 
     return history_query, ""
