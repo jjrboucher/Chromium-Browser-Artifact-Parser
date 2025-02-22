240730860276637f0fcd86d65ebfd4d92466db79
jjrboucher <jjrboucher@users.noreply.github.com>
2025-02-22 12:07:00 -0500

    Added parsing of dangerous_type field using values in the source code found here: https://source.chromium.org/chromium/chromium/src/+/main:components/download/public/common/download_danger_type.h?q=danger_type

--- SQLite/downloads.py
+++ SQLite/downloads.py
@@ -2,7 +2,7 @@ def chrome_downloads():
     worksheet = 'Downloads'
     sql_query = """
         /*
-        Last updated: 2021-10-10
+        Last updated: 2025-02-22
         Author:  Jacques Boucher - jjrboucher@gmail.com
         Tested with: Chrome 94
         Only changes from previous query that was tested with Chrome 71 and 78 is that a few
@@ -26,27 +26,62 @@ def chrome_downloads():
                 total_bytes,
                 start_time, 
                 datetime(start_time/1000000-11644473600,'unixepoch') AS "Decoded start_time (UTC)", 
+                
                 end_time,
                 CASE
                    WHEN end_time>0 THEN datetime(end_time/1000000-11644473600,'unixepoch')
                    ELSE 0
                 END AS "Decoded end_time (UTC)",
+                
                 last_access_time,
                 CASE 
                    WHEN last_access_time>0 THEN datetime(last_access_time/1000000-11644473600,'unixepoch')
                    ELSE 'Not opened via Chrome'
                 END AS "Decoded last_access_time (UTC)",
+                
                 last_modified, 
                 referrer,
                 site_url, 
                 tab_url, 
                 tab_referrer_url,
+                
                 state,
                 CASE state 
                    WHEN 1 THEN "Complete" 
                    WHEN 2 THEN "Interrupted"
                    ELSE "New value!: "||state||" Check source code for meaning!"
                 END AS "Decoded state",
+                
+                danger_type,
+                /* see https://source.chromium.org/chromium/chromium/src/+/main:components/download/public/common/download_danger_type.h?q=danger_type */
+                CASE danger_type
+                    WHEN 0 THEN "Not Dangerous"
+                    WHEN 1 THEN "Dangerous File"
+                    WHEN 2 THEN "Dangerous URL"
+                    WHEN 3 THEN "Dangerous Content"
+                    WHEN 4 THEN "Maybe Dangerous Content"
+                    WHEN 5 THEN "Uncommon Content"
+                    WHEN 6 THEN "User Validated"
+                    WHEN 7 THEN "Dangerous Host"
+                    WHEN 8 THEN "Potentially Unwanted"
+                    WHEN 9 THEN "Allow Listed by Policy"
+                    WHEN 10 THEN "ASYNC Scanning"
+                    WHEN 11 THEN "Blocked Password Protected"
+                    WHEN 12 THEN "Blocked too Large"
+                    WHEN 13 THEN "Sensitive Content Warning"
+                    WHEN 14 THEN "Sensitive Content Block"
+                    WHEN 15 THEN "Deep Scanned Safe"
+                    WHEN 16 THEN "Deep Scanned Opened Dangerous"
+                    WHEN 17 THEN "Prompt for Scanning"
+                    WHEN 18 THEN "Blocked Unsupported" /* Deprecated */
+                    WHEN 19 THEN "Dangerous Account Compromise"
+                    WHEN 20 THEN "Deep Scanned Failed"
+                    WHEN 21 THEN "Prompt for Local Password Scanning"
+                    WHEN 22 THEN "ASYNC Local Password Scanning"
+                    WHEN 23 THEN "Blocked Scan Failed"
+                ELSE "New value!: "||danger_type||" Check source code for meaning!"
+                END AS "Decoded danger_type",
+                
                 interrupt_reason,
                 /* 
                 see https://source.chromium.org/chromium/chromium/src/+/main:components/download/public/common/download_interrupt_reason_values.h
@@ -91,6 +126,7 @@ def chrome_downloads():
                    WHEN 50 THEN "Browser crashed"
                    ELSE "New value!: "||interrupt_reason||" Check source code for meaning!"
                 END AS "Decoded interrupt_reason",
+                
                 chains.url AS "Download Source"
 
         FROM downloads
