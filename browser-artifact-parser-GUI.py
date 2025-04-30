a993ff9135869a9044456604a3fa7d334de2bf2b
jjrboucher <jjrboucher@users.noreply.github.com>
2025-04-29 21:57:22 -0400

    Updated version date in comments

--- browser-artifact-parser-GUI.py
+++ browser-artifact-parser-GUI.py
@@ -1,6 +1,6 @@
 # Written by Jacques Boucher
 # email: jjrboucher@gmail.com
-# version date: 2025-Mar-05
+# version date: 2025-Apr-29
 #
 # Script to extract data from Google Chrome's or MS Edge's SQLite databases
 # Outputs to an Excel file.
